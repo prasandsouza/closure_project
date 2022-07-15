6630fe61fc1da89dd8c308dcd94bf8d9686a4d0a
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-15 14:05:33 +0530

    adding function file

---
+++ counterFactory.js
@@ -0,0 +1,15 @@
+function factoryfunction(){
+    
+    return {
+    incrementmenthod(){
+        number = number + 1;
+        return number
+    },
+    decrementmentmethod(){
+        number = number - 1
+        return number
+    }
+    }
+}
+module.exports = factoryfunction()
+let number = 10;
