b940dedfcd6945e8635c61f6e99420f79afca0e8
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-15 14:07:23 +0530

    adding testcase file for counter project

---
+++ testfile/testcounterFactory.js
@@ -0,0 +1,5 @@
+let counterfactory = require('../counterFactory')
+counterfactory.decrementmentmethod();
+counterfactory.incrementmenthod()
+console.log(counterfactory.decrementmentmethod())
+console.log(counterfactory.incrementmenthod())
